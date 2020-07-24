7babc40633cc4260540d6b9c93b727dfc11cf113
Praveen <praveen.babu36@gmail.com>
2020-07-24 13:31:25 +0530

    Solution 2 - Read file as Chunks using ReadStream and send Reponse

--- 2-how-node-works/streams.js
+++ 2-how-node-works/streams.js
@@ -1,13 +1,32 @@
 const fs = require("fs");
 const http = require("http");
+const { Readable } = require("stream");
 
 const server = http.createServer();
 
 server.on("request", (req, res) => {
-  // Solution 1
-  fs.readFile("test-file.txt", (err, data) => {
+  // Solution 1 - This loads the entire file content to memory
+  //   and send it to response
+  /*fs.readFile("test-file.txt", (err, data) => {
     if (err) console.log(err);
-    res.send(data);
+    res.end(data);
+  });*/
+
+  // Solution 2 - Using streams
+  const readStream = fs.createReadStream("test-file.txt");
+
+  readStream.on("data", (chunk) => {
+    res.write(chunk);
+  });
+
+  readStream.on("end", () => {
+    res.end();
+  });
+
+  readStream.on("error", (err) => {
+    console.log(err);
+    res.statusCode = 500;
+    res.end("File not found!");
   });
 });
 
